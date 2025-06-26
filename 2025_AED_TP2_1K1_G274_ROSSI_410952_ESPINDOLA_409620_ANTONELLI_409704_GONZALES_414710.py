980c49a70ce434d3364248086e2e837b4dc622e8
Matias <matiii.rossi2003@gmail.com>
2025-06-26 15:27:05 -0300

    agregue la funcion del monto_base y el monto_final (incompleta).

--- 2025_AED_TP2_1K1_G274_ROSSI_410952_ESPINDOLA_409620_ANTONELLI_409704_GONZALES_414710.py
+++ 2025_AED_TP2_1K1_G274_ROSSI_410952_ESPINDOLA_409620_ANTONELLI_409704_GONZALES_414710.py
@@ -1,4 +1,57 @@
-# detecta el tipo de moneda en el codigo de orden...
+# calcular monto final...
+def calcular_monto_final(base, id_impuesto):
+    if base <= 500000:
+        monto_final = round(monto_base - monto_base * 21 / 100, 2)
+    else:
+        monto_final = monto_base
+
+
+# calcular monto base...
+def calcular_monto_base(mon, nominal, id_comision):
+    monto_base = nominal
+    # algoritmo 1...
+    if id_comision == 1 and mon == 'ARS':
+        comision = nominal * 9 / 100
+    
+    # algoritmo 2...
+    elif id_comision == 2 and mon == 'USD':
+        if nominal < 50000:
+            comision = 0
+        elif 50000 <= nominal < 80000:
+            comision = nominal * 5 / 100
+        elif nominal > 80000:
+            comision = nominal * 7.8 / 100
+    
+    # algoritmo 3...
+    elif id_comision == 3 and mon == 'EUR' or 'GBP':
+        monto_fijo = 100
+        if nominal > 25000:
+            comision = nominal * 6 / 100
+
+    # algoritmo 4...
+    if id_comision == 4 and nominal <= 100000 and mon == 'JPY':
+        comision = 500
+    if id_comision == 4 and nominal > 100000:
+        comision = 1000
+
+    # algoritmo 5...
+    if id_comision == 5 and nominal < 500000 and mon == 'ARS':
+        comision = 0
+    elif id_comision == 5 and nominal >= 500000 and mon == 'ARS':
+        comision = nominal * 7 / 100
+    if id_comision == 5 and comision > 50000 and mon == 'ARS':
+        comision = 50000
+
+    # calculo del monto base...
+    if monto_fijo:
+        monto_base = nominal - (monto_fijo + comision)
+    else:
+        monto_base = nominal - comision
+    
+    return monto_base
+
+
+# valida el tipo de moneda...
 def tipo_moneda(cod_orden):
     # flag ya hay un tipo de moneda...
     smon = False
@@ -29,67 +82,38 @@ def tipo_moneda(cod_orden):
         moneda = 'Moneda incorrecta'
     return moneda
 
+
+# valida destinatario...
 def destinatario_valido(cod_ide):
-    #elimina espacios en blanco
-    cod_ide = cod_ide.strip()
+    cod_ide = cod_ide.strip()  # elimina espacios en blanco...
+    # valida que no este vacio el campo...
     if len(cod_ide) == 0:
         return False
-    tiene_letra_o_digito = False
+    tiene_letra_o_digito = False  # flag...
     for c in cod_ide:
         if c.isupper() or c.isdigit():
             tiene_letra_o_digito = True
         elif c != '-':
             return False
-    if not tiene_letra_o_digito:
+    if '-' in cod_ide and not tiene_letra_o_digito:
+        return False
+    elif not tiene_letra_o_digito:
         return False
     return True
 
 
-# calcular monto base...
-def monto_base(moneda):
-    # ARS...
-    monto_base = round(monto_nominal - monto_nominal * 5 / 100, 2)
-    # USD...
-    monto_base = round(monto_nominal - monto_nominal * 7 / 100, 2)
-    # EUR...
-    monto_base = round(monto_nominal - monto_nominal * 7 / 100, 2)
-    # GBP...
-    monto_base = round(monto_nominal - monto_nominal * 9 / 100, 2)
-    # JPY...
-    # enunciado adicional TP1...
-    if 15000 <= monto_nominal <= 500000:
-        monto_base = round(monto_nominal - monto_nominal * 9 / 100, 2)
-    elif 500000 < monto_nominal <= 1500000:
-        monto_base = round(monto_nominal - monto_nominal * 7.8 / 100, 2)
-    elif 1500000 < monto_nominal <= 10000000:
-        monto_base = round(monto_nominal - monto_nominal * 5.5 / 100, 2)
-    elif 10000000 < monto_nominal:
-        monto_base = round(monto_nominal - monto_nominal * 5 / 100, 2)
-    # Moneda incorrecta...
-    else:
-        monto_base = 0
-
-
-# calcular monto final...
-def monto_final():
-    if monto_base > 500000:
-        monto_final = round(monto_base - monto_base * 21 / 100, 2)
-    else:
-        monto_final = monto_base
-
-
 def principal():
-    #cantidad para los resultados
-    cant_moneda_invalida = 0
-    cant_dest_invalido = 0
-    cant_operaciones_validas = 0  # <-- agregado para (r3)
-
     # definicion de variables...
     nom_dest = cod_ide = cod_op = monto_nominal = id_calc_com = id_calc_imp = ''
 
+    # contadores para los resultados...
+    cant_minvalida = 0  # cantidad moneda invalida...
+    cant_binvalido = 0  # cantidad beneficiario invalido...
+    cant_oper_validas = 0  # cantidad operaciones validas...
+
     # procesamiento del archivo ordenes.txt...
     m = open('ordenes.txt', 'rt')
-    timestamp = m.readline()  # ignorar el timestamp...
+    timestamp = m.readline()  # ignora el timestamp...
 
     for linea in m:
         # procesamiento linea por linea de las ordenes de pago...
@@ -104,15 +128,17 @@ def principal():
         # procesos...
         moneda = tipo_moneda(cod_orden)
         if moneda == 'Moneda incorrecta':
-            cant_moneda_invalida += 1
-            continue
+            cant_minvalida += 1
 
-        if not destinatario_valido(cod_ide):
-            cant_dest_invalido += 1
-            continue
+
+        elif not destinatario_valido(cod_ide):
+            cant_binvalido += 1
+
 
          # Aquí está la suma del punto (r3):
-        cant_operaciones_validas += 1
+        elif destinatario_valido(cod_ide):
+            cant_oper_validas += 1
+
     # cerrar el .txt
     m.close()
 
@@ -121,7 +147,7 @@ def principal():
     print(' (r2) - Cantidad de ordenes invalidas - beneficiario mal identificado:', cant_binvalido)
     print(' (r3) - Cantidad de operaciones validas:', cant_oper_validas)
     print(' (r4) - Suma de montos finales de operaciones validas:', suma_mf_validas)
-    print(' (r5) - Cantidad de ordenes para moneda ARS:', cant_ARS)
+"""    print(' (r5) - Cantidad de ordenes para moneda ARS:', cant_ARS)
     print(' (r6) - Cantidad de ordenes para moneda USD:', cant_USD)
     print(' (r7) - Cantidad de ordenes para moneda EUR:', cant_EUR)
     print(' (r8) - Cantidad de ordenes para moneda GBP:', cant_GBP)
@@ -133,6 +159,6 @@ def principal():
     print('(r14) - Cantidad de veces que apareció ese mismo nombre:', cant_nom_primer_benef)
     print('(r15) - Porcentaje de operaciones inválidas sobre el total:', porcentaje)
     print('(r16) - Monto final promedio de las ordenes validas en moneda ARS:', promedio)
-
+"""
 
 principal()
